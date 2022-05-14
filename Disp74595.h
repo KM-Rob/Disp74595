a38af44e80773ce369556f4cb1cca0e532288e76
KM-Rob <66571687+KM-Rob@users.noreply.github.com>
2022-05-14 22:06:01 +0200

    Add files via upload
    Dodano komentarze w pliku h, dodano podświetlanie składni (plik keywords), zmiejszono czas opóźnień przy obsłudze output enable do poziomu mikrosekund

--- Disp74595.h
+++ Disp74595.h
@@ -9,17 +9,23 @@
 class Disp74595
 {
 	private:
-	int num;
-	int typ;
-	char oute;	//output enable pin
+	uint8_t num;
+	uint8_t typ;
+	uint8_t oute;	//output enable pin
 	Rej74595 rej[MAXREG];
 	
 	public:
-	begin(int NO, int type,char latch, char outen, int delau);
-	WriteDisplay(int val);
-	StepDisplay(int val);
+	// NO - liczba wyswietlaczy
+	// pozostale parametry jak w bibliotece Rej595
+	begin(uint8_t NO, uint8_t type, uint8_t latch, uint8_t outen, int delau);
+	// wyswietla na wszystkich wyswietlaczach ta sama wartosc - val
+	WriteDisplay(uint8_t val);
+	// wyswietla wartosc val na pierwszym wyswietlaczu a na reszcie 0
+	StepDisplay(uint8_t val);
+	// przesuwa wartosci wyswietlaczy o 1 i wyswietla
 	LoopDisplay(void);
-	int ReadFirst(void);
+	// odczytuje aktualna wartosc z pierwszego wyswietlacza
+	uint8_t ReadFirst(void);
 };
 
 
